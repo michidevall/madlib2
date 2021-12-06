aa381e95040e66def0939c16694920ffe3507000
michidevall <glitzpixy1@yahoo.com>
2021-12-06 11:31:38 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+# string concatenation (aka hot to put stings together)
+# suppose we want to create a string that says " subscribe to____"
+youtuber = ""
+
+#  a few ways to do this
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
